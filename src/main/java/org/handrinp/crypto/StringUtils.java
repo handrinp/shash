bced1bbed9961d29b0e6b83638cfa54e578b948a
Nick Handrick <handrinp@uwec.edu>
2017-07-08 21:08:37 -0500

    use built in string and byte conversions

--- src/main/java/org/handrinp/crypto/StringUtils.java
+++ src/main/java/org/handrinp/crypto/StringUtils.java
@@ -1,23 +1,15 @@
 package org.handrinp.crypto;
 
+import java.nio.charset.Charset;
+
 public class StringUtils {
+	public static final Charset CHARSET = Charset.defaultCharset();
+
 	public static String fromBytes(byte[] bytes) {
-		char[] chars = new char[bytes.length];
-
-		for (int i = 0; i < bytes.length; ++i) {
-			chars[i] = (char) bytes[i];
-		}
-
-		return new String(chars);
+		return new String(bytes, CHARSET);
 	}
 
 	public static byte[] fromString(String string) {
-		byte[] bytes = new byte[string.length()];
-
-		for (int i = 0; i < bytes.length; ++i) {
-			bytes[i] = (byte) string.charAt(i);
-		}
-
-		return bytes;
+		return string.getBytes(CHARSET);
 	}
 }
