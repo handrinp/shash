da218fce62202870efa8798ceff0ca25deebf505
Nick Handrick <handrinp@uwec.edu>
2017-07-08 20:11:55 -0500

    whitespace

--- src/main/java/org/handrinp/crypto/Shash.java
+++ src/main/java/org/handrinp/crypto/Shash.java
@@ -2,19 +2,19 @@ package org.handrinp.crypto;
 
 public class Shash {
 	private Hash hash;
-	
+
 	public Shash(Hash hash) {
 		this.hash = hash;
 	}
-	
+
 	public void setHash(Hash hash) {
 		this.hash = hash;
 	}
-	
+
 	public Hash getHash() {
 		return hash;
 	}
-	
+
 	public String hash(String input) {
 		byte[] hashable = StringUtils.fromString(input);
 		byte[] hashed = hash.hash(hashable);
